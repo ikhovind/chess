e53733a813201ce441faea91619a4db01e162c01
Ingebrigt Hovind <ing.hovind@hotmail.com>
2022-06-11 15:18:39 +0200

    bitboard forward pawn moves + takes right

---
+++ src/game.rs
@@ -0,0 +1,68 @@
+use crate::game::board_consts::{FILE_A, FILE_H, RANK_4, RANK_8};
+use crate::print_u64_bitboard;
+
+mod board_consts;
+
+//[black, white]
+pub struct Board {
+    pub(crate) pawns: [u64; 2],
+    pub(crate) knights: [u64; 2],
+    pub(crate) bishops: [u64; 2],
+    pub(crate) rooks: [u64; 2],
+    pub(crate) queens: [u64; 2],
+    pub(crate) kings: [u64; 2],
+    black_pieces: u64,
+    empty: u64,
+}
+
+impl Board {
+    pub fn new(_pawns :[u64; 2], _knights :[u64; 2], _bishops :[u64; 2], _rooks :[u64; 2], _queens :[u64; 2], _kings: [u64; 2]) -> Board {
+        let empty = !_pawns[0] & !_pawns[1] & !_knights[0] & !_knights[1] & !_bishops[0] & !_bishops[1] & !_rooks[0] & !_rooks[1] & !_queens[0] & !_queens[1] & !_kings[0] & !_kings[1];
+        let black = _pawns[0] | _pawns[1] | _knights[0] | _knights[1] | _bishops[0] | _bishops[1] | _rooks[0] | _rooks[1] | _queens[0] | _queens[1] | _kings[0] | _kings[1];
+        return Board {pawns: _pawns, knights: _knights, bishops: _bishops, rooks: _rooks, queens: _queens, kings: _kings, black_pieces: black, empty}
+    }
+
+    pub fn possible_pw(&mut self, history: String) -> String {
+        let mut list = String::from("");
+
+        let mut pawn_moves = (self.pawns[1] << 9) & (self.black_pieces) & (!RANK_8) & (!FILE_A); // capture right
+
+        for i in 0..64 {
+            if ((pawn_moves >> i) & 1) == 1 {
+                //print!("{} {} {} {}", (i / 8), (i % 8),(i / 8 + 1), (i % 8 + 1));
+                list.push_str(&*((i / 8) + (i % 8 + 1) + (i / 8 + 1) + (i % 8 + 1)).to_string());
+            }
+        }
+
+        pawn_moves=(self.pawns[1] << 7)&self.black_pieces&!RANK_8&!FILE_H;//capture left
+        for i in 0..64 {
+            if ((pawn_moves>>i)&1)==1 {list.push_str(&*((i/8+1)+(i%8+1)+(i/8)+(i%8)).to_string());}
+        }
+        pawn_moves=(self.pawns[1] << 8)&self.empty&!RANK_8;//move 1 forward
+        for i in 0..64 {
+            if ((pawn_moves>>i)&1)==1 {
+                list.push_str(&*((i/8+1)+(i%8)+(i/8)+(i%8)).to_string());
+            }
+        }
+        pawn_moves=(self.pawns[1] << 16)&self.empty&(self.empty << 8)&RANK_4;//move 2 forward
+        for i in 0..64 {
+            if ((pawn_moves>>i)&1)==1 {
+                list.push_str(&*((i/8+2)+(i%8)+(i/8)+(i%8)).to_string());
+            }
+        }
+        //y1,y2,Promotion Type,"P"
+        pawn_moves=(self.pawns[1] << 7)&self.black_pieces&RANK_8&!FILE_A;//pawn promotion by capture right
+        for i in 0..64 {
+            if ((pawn_moves>>i)&1)==1 {list.push_str(&*format!("{}{}{}{}{}{}{}{}", (i % 8 - 1) + (i % 8), "QP", (i % 8 - 1) + (i % 8), "RP", (i % 8 - 1) + (i % 8), "BP", (i % 8 - 1) + (i % 8), "NP"));}
+        }
+        pawn_moves=(self.pawns[1] << 9)&self.black_pieces&RANK_8&!FILE_H;//pawn promotion by capture left
+        for i in 0..64 {
+            if ((pawn_moves>>i)&1)==1 {list.push_str(&*format!("{}{}{}{}{}{}{}{}", (i%8+1)+(i%8),"QP",(i%8+1)+(i%8),"RP",(i%8+1)+(i%8),"BP",(i%8+1)+(i%8),"NP"))}
+        }
+        pawn_moves=(self.pawns[1] << 8)&self.empty&RANK_8;//pawn promotion by move 1 forward
+        for i in 0..64 {
+            if ((pawn_moves>>i)&1)==1 {list.push_str(&*format!("{}{}{}{}{}{}{}{}",(i%8)+(i%8),"QP",(i%8)+(i%8),"RP",(i%8)+(i%8),"BP",(i%8)+(i%8),"NP"));}
+        }
+        return list;
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,97 @@
+pub mod game;
+
+use std::env;
+
+fn print_u64_bitboard(bitboard : u64) {
+    println!();
+    let mut c = 0;
+    let mut d = 0;
+    let mut str = String::from("");
+    for i in format!("{:#066b}", bitboard).chars() {
+        if d > 1 {
+            str.push(i);
+            c +=1;
+            if c == 8 {
+                print!("{}", str.chars().rev().collect::<String>());
+                print!("\n");
+                str = String::from("");
+                c = 0;
+            }
+        }
+        else {
+            d += 1;
+        }
+    }
+}
+fn main() {
+    let mut _pawns = [0,0];
+    let mut _bishops = [0,0];
+    let mut _rooks = [0,0];
+    let mut _knights = [0,0];
+    let mut _queens = [0,0];
+    let mut _kings = [0,0];
+
+    let args: Vec<String> = env::args().collect();
+    let mut column : u32 = 0;
+    let mut row = 7;
+    let mut res = 0;
+    let mut white = 1;
+    for i in args[1].chars() {
+        if i.is_alphabetic() {
+            res = 2_u64.pow((column + row * 8) as u32);
+            //print_u64_bitboard(res);
+        }
+        if !i.is_uppercase() {
+            white = 0;
+        }
+        else {
+            white = 1
+        }
+        match i.to_ascii_lowercase() {
+            'p' => {
+                column += 1;
+                _pawns[white] |= res;
+            }
+            'r' => {
+                column += 1;
+                _rooks[white] |= res;
+            }
+            'n' => {
+                column += 1;
+                _knights[white] |= res;
+            }
+            'q' => {
+                column += 1;
+                _queens[white] |= res;
+            }
+            'k' => {
+                column += 1;
+                _kings[white] |= res;
+            }
+            'b' => {
+                column += 1;
+                _bishops[white] |= res;
+            }
+            '/' => {
+                row-=1;
+                column = 0;
+            }
+            ' ' => {
+                break;
+            }
+            i if i.is_numeric() => {
+                column += i.to_digit(10).unwrap();
+            }
+            _ => {}
+        }
+    }
+    let mut board = game::Board::new(
+        _pawns,
+        _knights,
+        _bishops,
+        _rooks,
+        _queens,
+        _kings
+    );
+    board.possible_pw( String::from(""));
+}
