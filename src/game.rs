78692cb96ad1297b1340f77123d716d216c61efa
Ingebrigt Hovind <ing.hovind@hotmail.com>
2022-06-12 18:08:41 +0200

    king moves

--- src/game.rs
+++ src/game.rs
@@ -239,6 +239,59 @@ impl Board {
         return list;
     }
 
+    /* LookupTables is a structure which contains all precomputed lookup tables */
+    pub fn possible_k(&self, white: bool) -> Vec<Move> {
+        /* we can ignore the rank clipping since the overflow/underflow with
+            respect to rank simply vanishes. We only care about the file
+            overflow/underflow. */
+        let mut opposing_pieces: u64 = self.white_pieces;
+        let mut own_pieces = self.black_pieces;
+        let mut index = 0;
+        if white {
+            opposing_pieces = self.black_pieces;
+            own_pieces = self.white_pieces;
+            index = 1;
+        }
+        let mut list: Vec<Move> = Vec::new();
+        let kings = self.kings[index];
+
+        for i in 0u8..64u8 {
+            if 2_u64.pow(i as u32) & kings != 0 {
+                println!("king loc {}", i);
+                let king_loc = 1 << i;
+                let king_clip_file_h = king_loc & !FILE_MASKS[7];
+                let king_clip_file_a = king_loc & !FILE_MASKS[0];
+
+                /* remember the representation of the board in relation to the bitindex
+                    when looking at these shifts.... */
+                let spot_1 = king_clip_file_h << 7;
+                let spot_2 = king_loc << 8;
+                let spot_3 = king_clip_file_h << 9;
+                let spot_4 = king_clip_file_h << 1;
+
+                let spot_5 = king_clip_file_a >> 7;
+                let spot_6 = king_loc >> 8;
+                let spot_7 = king_clip_file_a >> 9;
+                let spot_8 = king_clip_file_a >> 1;
+
+                let moves = (spot_1 | spot_2 | spot_3 | spot_4 | spot_5 | spot_6 |
+                    spot_7 | spot_8) & !own_pieces;
+
+                /* compute only the places where the king can move and attack. The caller
+                    will interpret this as a white or black king. */
+                for i2 in 0u8..64u8 {
+                    if 2u64.pow(i2 as u32) & moves != 0 {
+                        list.push(
+                            Move::new_move(i, i2, opposing_pieces & 2_u64.pow(i2 as u32) != 0)
+                        );
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+
     pub fn possible_n(&self, white: bool) -> Vec<Move> {
     /* we can ignore the rank clipping since the overflow/underflow with
         respect to rank simply vanishes. We only care about the file
