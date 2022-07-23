8daf65eba22a44f23180f04e729d0bbb8e7615f6
Ingebrigt Hovind <ing.hovind@hotmail.com>
2022-07-23 23:48:55 +0200

    simple websocket for uci

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 use std::fs::{File, OpenOptions};
-use std::io::{Write};
+use std::io;
+use std::io::{Read, Write};
+use std::net::{TcpListener, Shutdown, TcpStream};
 use std::path::Path;
+use std::process::exit;
 use std::time::{Instant, SystemTime};
 use chrono::{DateTime, Utc};
 use num_format::{Locale, WriteFormatted};
 use crate::game::Board;
 use crate::opponent::engine;
+extern crate vampirc_uci;
+use simple_websockets::{Event, Responder};
+use std::collections::HashMap;
 
+use std::thread;
+use vampirc_uci::{MessageList, parse, parse_with_unknown, UciMessage, UciTimeControl};
 pub mod game;
 mod mv;
 mod consts;
 mod move_gen;
 mod opponent;
+
+//static mut GAME: Board = Board::from_fen(String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"));
 
 fn print_u64_bitboard(bitboard: u64) {
     println!();
@@ -89,12 +99,121 @@ fn iso8601(st: &std::time::SystemTime) -> String {
     // formats like "2001-07-08T00:34:60.026490+09:30"
 }
 
+
 fn main() {
-    let mut b = Board::from_fen(
-        String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
-    );
+    // listen for WebSockets on port 8080:
+    let event_hub = simple_websockets::launch(8080)
+        .expect("failed to listen on port 8080");
+    // map between client ids and the client's `Responder`:
+    let mut clients: HashMap<u64, Responder> = HashMap::new();
 
-    println!("{}", engine::eval(&b));
+    loop {
+        match event_hub.poll_event() {
+            Event::Connect(client_id, responder) => {
+                println!("A client connected with id #{}", client_id);
+                // add their Responder to our `clients` map:
+                clients.insert(client_id, responder);
+            },
+            Event::Disconnect(client_id) => {
+                println!("Client #{} disconnected.", client_id);
+                // remove the disconnected client from the clients map:
+                clients.remove(&client_id);
+            },
+            Event::Message(client_id, message) => {
+                println!("Received a message from client #{}: {:?}", client_id, message);
+                // retrieve this client's `Responder`:
+                let responder = clients.get(&client_id).unwrap();
+                // echo the message back:
+                responder.send(message);
+            },
+        }
+    }
+}
 
-    //test(String::from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"), 6);
+fn parser(input: &str) -> Result<&str, &str> {
+    let messages: MessageList = parse(&input);
+
+    for m in messages {
+        println!("m: {}", m);
+        match m {
+            UciMessage::Uci => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Position { startpos, fen, moves } => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Go { time_control, search_control } => {
+                if let Some(tc) = time_control {
+                    match tc {
+                        UciTimeControl::Ponder => {
+                            return Result::Err("Not implemented yet ");
+                        }
+                        UciTimeControl::TimeLeft { white_time, white_increment, black_time, black_increment, moves_to_go } => {
+                            return Result::Err("Not implemented yet ");
+                        }
+                        UciTimeControl::Infinite => {
+                            return Result::Err("Not implemented yet ");
+                        }
+                        UciTimeControl::MoveTime(duration) => {
+                            return Result::Err("Not implemented yet ");
+                        }
+                    }
+                }
+            }
+            UciMessage::IsReady => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::UciNewGame => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Stop => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::PonderHit => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Quit => {
+                return Result::Err("Not implemented yet ");
+            }
+
+            UciMessage::Debug(_) => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Register { .. } => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::SetOption { .. } => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Id { .. } => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::UciOk => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::ReadyOk => {
+                return Result::Err("Not implemented yet ");
+
+            }
+            UciMessage::BestMove { .. } => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::CopyProtection(_) => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Registration(_) => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Option(_) => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Info(_) => {
+                return Result::Err("Not implemented yet ");
+            }
+            UciMessage::Unknown(_, _) => {
+                return Result::Err("Not implemented yet ");
+            }
+        }
+    }
+    return Result::Err("No message");
 }
